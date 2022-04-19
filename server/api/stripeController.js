ba6af2e141a4f668982cf68d7204672f9ee0a50b
gitdevstar <56650395+gitdevstar@users.noreply.github.com>
2022-04-19 15:59:52 -0400

    sort out

--- server/api/stripeController.js
+++ server/api/stripeController.js
@@ -4,24 +4,6 @@ const sdk = require('api')('@coinbase-exchange/v1.0#10ldz4jl0h5qqgo');
 
 class Controller {
 
-    async checkout(req) {
-        const amount = req.amount;
-        const coin = req.coin;
-        const wallet = req.wallet;
-        var payable = 0.0;
-            if (coin === 'eth') {
-                await sdk['ExchangeRESTAPI_GetCoinbasePriceOracle']()
-                .then(res => {
-                    payable = amount / res['prices'['ETH']];
-                })
-                .catch(err => console.error(err));
-            } else {
-                payable = amount;
-            }
-
-        return client.rest.withdraw.withdrawToCryptoAddress(payable, coin, wallet, 'buy', true);
-    }
-
     async payout(req) {
         try {
             const dest = req.bankId;
